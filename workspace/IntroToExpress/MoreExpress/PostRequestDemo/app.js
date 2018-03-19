3db42165c1de92eec0278d01995e612602e64f12
Rojin Bijukchhe <ro_jin25@icloud.com>
2018-03-19 02:54:38 +0000

    added

--- workspace/IntroToExpress/MoreExpress/PostRequestDemo/app.js
+++ workspace/IntroToExpress/MoreExpress/PostRequestDemo/app.js
@@ -3,9 +3,19 @@ var app = express();
 
 app.set("view engine", "ejs");
 
-app.set("/",function(req, res){
-    res.render("Home");
-})
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/friends", function(req, res){
+    var friends = ["Tony", "Ronney","Justin", "Lily"];
+    res.render("friends", {friends: friends});
+});
+
+app.post("/addfriend", function(req, res){
+    res.send("YOU HAVE REACHED THE POST ROUTE");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
 });
