2448f21494687e3f885968b6095f0571d1d35f95
Rojin Bijukchhe <ro_jin25@icloud.com>
2018-03-19 01:59:23 +0000

    Added some new files PostRequestDemo

--- workspace/IntroToExpress/MoreExpress/PostRequestDemo/app.js
+++ workspace/IntroToExpress/MoreExpress/PostRequestDemo/app.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.set("/",function(req, res){
+    res.render("Home");
+})
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
 });
